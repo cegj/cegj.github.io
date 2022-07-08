cf2fbf472eaa5afefb821df389b7fc3b5857c4f1
cegj <cegasparjr@gmail.com>
2022-07-08 17:32:48 -0300

    Set sidebox info and style adjustments

---
+++ assets/script/script.js
@@ -0,0 +1,29 @@
+class Resume{
+  constructor(altHeaderSelector){
+    this.altHeader = document.querySelector(altHeaderSelector);
+  }
+
+  showHeader(){
+    if (window.pageYOffset >= 408){
+      this.altHeader.dataset.show = "true";
+    } else {
+      this.altHeader.dataset.show = "false";
+    }
+  }
+
+  addEvents(){
+    window.addEventListener("scroll", this.showHeader)
+  }
+
+  bindEvents(){
+    this.showHeader = this.showHeader.bind(this);
+  }
+
+  init(){
+    this.bindEvents();
+    this.addEvents();
+  }
+}
+
+const resume = new Resume('.side-box');
+resume.init();
